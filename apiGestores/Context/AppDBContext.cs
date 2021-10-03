62f350f868899e58575f2b628b3c61f6937a794b
Rafael Martinez <rafaelvady.q@gmail.com>
2021-10-03 00:22:23 -0600

    first commit

---
+++ apiGestores/Context/AppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using apiGestores.Moldels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiGestores.Context
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Gestores_bd> gestores_bd;
+    }
+}
---
+++ apiGestores/Moldels/Gestores_bd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace apiGestores.Moldels
+{
+    public class Gestores_bd
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int lanzamiento { get; set; }
+        public string desarrollo { get; set; }
+    }
+}
